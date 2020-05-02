8d6fb65b183f92e72f1a6c59f1fb922133e7399b
adecess <axel.saliniere@gmail.com>
2020-05-02 12:07:43 -0400

    tweaked POST request

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import postRecipe from './request'
 
-console.log(postRecipe(45));
+// post body data 
+const data = {
+    user_id: 140,
+    from: "http://iletaitungateau.com/articles/356"
+};
+
+console.log(postRecipe('https://cors-anywhere.herokuapp.com/https://fraaise2.herokuapp.com/api/recommendations/45', data));
--- src/request.js
+++ src/request.js
@@ -1,27 +1,17 @@
-// post body data 
-const user = {
-    user_id: 140,
-    from: "http://iletaitungateau.com/articles/356"
-};
-
-// request options
-const options = {
-    method: 'POST',
-    mode: 'cors',
-    headers:{
-        'Access-Control-Allow-Origin':'*'
-    },
-    body: JSON.stringify(user)
-}
-
-const postRecipe = async (id_site) => {
-    const response = await fetch(`https://fraaise2.herokuapp.com/api/recommendations/${id_site}`, options);
-
-    if(response.status === 200) {
-        const data = await response.json();
-        return data;
-    } else {
-        throw new Error('Unable to fetch data');
+const postRecipe = async (url = '', data = {}) => {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const returnData = await response.json();
+        console.log(returnData);
+    } catch (error) {
+        console.log('C\'est la merde mon coco!');
     }
 }
 
