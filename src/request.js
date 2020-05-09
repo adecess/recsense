65903e39edbf348eb0ea63afb1ba738535d0df9b
adecess <axel.saliniere@gmail.com>
2020-05-09 11:47:07 -0400

    fixed request issue

--- src/request.js
+++ src/request.js
@@ -2,11 +2,10 @@ const postRecipe = async (url = '', data = {}) => {
     try {
         const response = await fetch(url, {
             method: 'POST',
-            mode: 'cors',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(data),
         });
         const returnData = await response.json();
         console.log(returnData);
