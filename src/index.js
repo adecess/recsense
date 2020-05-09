3ea82db24fedf4a6c3739e316c73e63d20f5c094
Kévin Joannon <kevin.joannon@outlook.fr>
2020-05-09 17:40:56 +0200

    Fix API call issue

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import postRecipe from './request'
 
 // post body data 
 const data = {
-    user_id: 140,
+    user_id: "140",
     from: "http://iletaitungateau.com/articles/356"
 };
 
-console.log(postRecipe('https://cors-anywhere.herokuapp.com/https://fraaise2.herokuapp.com/api/recommendations/45', data));
+console.log(postRecipe('https://fraaise2.herokuapp.com/api/recommendations/55', data));
